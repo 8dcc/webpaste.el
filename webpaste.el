192df2790e2996bb45980bcc46fce87d18febc9b
8dcc <8dcc.git@gmail.com>
2024-11-24 18:38:24 +0100

    Use 'region-beginning' and 'region-end' in 'webpaste-paste-buffer-or-region'
    Rather than 'point' and 'mark'.

--- webpaste.el
+++ webpaste.el
@@ -628,7 +628,7 @@ Takes optional POINT and MARK to paste a region."
   ;; if region is selected
   (if (use-region-p)
       ;; Paste selected region
-      (webpaste-paste-region point mark)
+      (webpaste-paste-region (region-beginning) (region-end))
     ;; Else, Paste buffer
     (webpaste-paste-buffer)))
 
